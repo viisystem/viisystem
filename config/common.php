497d43485d55d4ccab4d5ee66828e15567b6fe56
minhnguyenerp <minhnguyenerp@gmail.com>
2016-10-18 11:01:40 +0700

    Cookie salt

--- config/common.php
+++ config/common.php
@@ -7,7 +7,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'fjdslkfjsdklafjs83454',
+            'cookieValidationKey' => 'lwA4Hg541EcQiC7VCrybYDVi6j7X1sOyfwaoPFRI',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
