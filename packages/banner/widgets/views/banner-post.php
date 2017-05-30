0c438fc150f0bb8447c8be4c70cfbe706dddd5d1
Phongnv (Minion) <phongnv093@gmail.com>
2017-05-30 23:56:08 +0700

    Update banner

--- packages/banner/widgets/views/banner-post.php
+++ packages/banner/widgets/views/banner-post.php
@@ -7,6 +7,33 @@ use vii\helpers\FileHelper;
 <!-- SLIDE -->
 <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="<?= $item->title ?>">
 	<!-- MAIN IMAGE -->
-	<a href="<?= $item->link ?>" <?= $item->target == 1 ? 'target="_blank"' : null ?>><?= Yii::$app->imageCache->img(FileHelper::getUploadDir($item->image), '', ['class' => 'img-responsive', 'data-bgfit' => 'initial', 'data-bgposition' => 'center top', 'data-bgrepeat' => 'no-repeat']); ?></a>
+	<?= Yii::$app->imageCache->img(FileHelper::getUploadDir($item->image), '', ['class' => 'img-responsive', 'data-bgfit' => '100%', 'data-bgposition' => 'center center', 'data-bgrepeat' => 'no-repeat']); ?>
+
+	<div class="tp-caption revolution-ch1 sft start"
+		data-x="center"
+		data-hoffset="0"
+		data-y="100"
+		data-speed="1500"
+		data-start="500"
+		data-easing="Back.easeInOut"
+		data-endeasing="Power1.easeIn"
+		data-endspeed="300">
+		 
+	</div>
+
+	<!-- LAYER -->
+	<div class="tp-caption sft"
+	data-x="center"
+	data-hoffset="0"
+	data-y="370"
+	data-speed="1600"
+	data-start="2800"
+	data-easing="Power4.easeOut"
+	data-endspeed="300"
+	data-endeasing="Power1.easeIn"
+	data-captionhidden="off"
+	style="z-index: 6">
+	<!-- <a href="<?= $item->link ?>" <?= $item->target == 1 ? 'target="_blank"' : null ?> class="btn-u btn-brd btn-brd-hover btn-u-light">Xem Thêm</a> -->
+	</div>
 </li>
 <!-- END SLIDE -->
